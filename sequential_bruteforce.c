a6ca5980e1a782d09164402530e8b82ec4068082
Davide Modolo <davide.modolo98@gmail.com>
2023-01-23 22:12:22 +0100

    solving distance function

--- sequential_bruteforce.c
+++ sequential_bruteforce.c
@@ -3,24 +3,22 @@
 #include <math.h>
 
 // Structure for a point in any number of dimensions
-typedef struct
-{
+typedef struct{
     int *coordinates;
     int num_dimensions;
 } Point;
 
 // Calculates the Euclidean distance between two points
-double distance(Point p1, Point p2)
-{
+double distance(Point p1, Point p2){
     double sum = 0;
-    for (int i = 0; i < p1.num_dimensions; i++)
-    {
-        sum += (p1.coordinates[i] - p2.coordinates[i]) * (p1.coordinates[i] - p2.coordinates[i]);
+    int n = p1.num_dimensions;
+    for (int i = 0; i < n; i++){
+        sum += pow((p1.coordinates[i] - p2.coordinates[i]), 2);
     }
     return sqrt(sum);
 }
-int main(int argc, char *argv[])
-{
+
+int main(int argc, char *argv[]){
     Point* points;
     int num_points, num_dimensions;
     FILE* fp = fopen("points.txt", "r");
@@ -43,22 +41,27 @@ int main(int argc, char *argv[])
     }
     fclose(fp);
     printf("The number of points is %d", num_points);
-    
-    
+
+
+    //print the first point
+    printf("\nThe first point first coordinate is %d", points[0].coordinates[0]);
+    printf("\nThe second point first coordinate is %d", points[1].coordinates[0]);
     // Calculate the distance between each pair of points
-    double min_dist = distance(points[0], points[1]);
-    for (int i = 0; i < num_points; i++)
-    {
-        for (int j = i + 1; j < num_points; j++)
-        {
-            double dist = distance(points[i], points[j]);
-            if (dist < min_dist)
-            {
-                min_dist = dist;
-            }
-        }
-    }
-    printf("The closest pair of points' distance is %f", min_dist);
+    
+    Point p1;
+    p1.num_dimensions = 2;
+    p1.coordinates = (int *)malloc(2 * sizeof(int));
+    p1.coordinates[0] = 0;
+    p1.coordinates[1] = 1;
+
+    Point p2;
+    p2.num_dimensions = 2;
+    p2.coordinates = (int *)malloc(2 * sizeof(int));
+    p2.coordinates[0] = 1;
+    p2.coordinates[1] = 2;
+
+    double min_distance = distance(p1,p2);
+    printf("The minimum distance is %f", min_distance);
     // free the memory
     free(points);
  
