dabf451d584bf3cdef8b6d9682566cd22ec26145
Davide Modolo <davide.modolo98@gmail.com>
2023-01-23 23:06:00 +0100

    added info

--- sequential_bruteforce.c
+++ sequential_bruteforce.c
@@ -41,37 +41,59 @@ int main(int argc, char *argv[]){
         }
     }
     fclose(fp);
-    printf("The number of points is %d\n", num_points);
-    int p1 = 0, p2 = 1;
+    printf("Loaded %d points in %d dimensions\n", num_points, num_dimensions);
+    // count and save all points indeces with minimum distance
+    int *min_points1 = (int *)malloc(num_points * sizeof(int));
+    int *min_points2 = (int *)malloc(num_points * sizeof(int));
+    int points1_count = 0;
+    int points2_count = 0;
     int count = 0;
+    // Initialize the minimum distance to the distance between the first two points
     double min_distance = distance(points[0], points[1]);
+    // Find the minimum distance between any two points
     for (int i = 0; i < num_points; i++){
         for (int j = i + 1; j < num_points; j++){
             double dist = distance(points[i], points[j]);
+            // if distance is less than min_distance, update min_distance and reset counters
             if (dist < min_distance){
-                p1 = i;
-                p2 = j;
                 min_distance = dist;
+                
+                min_points1[0] = i;
+                min_points2[0] = j;
                 count = 1;
+
+                points1_count = 1;
+                points2_count = 1;
+
             }
+            // if distance is equal to min_distance, add the pair to the list
             else if (dist == min_distance){
+                min_points1[points1_count++] = i;
+                min_points2[points2_count++] = j;
                 count++;
             }
         }
     }
-    printf("The minimum distance is %f with points #%d and #%d. ", min_distance, p1, p2);
+    // print results
+    printf("The minimum distance is %f. ", min_distance);
     printf("There are %d pairs with this distance.\n", count);
-    printf("The first point coordinates are:\n");
-    for (int i = 0; i < num_dimensions; i++){
-        printf("%d ", points[p1].coordinates[i]);
-    }
-    printf("\nThe second point coordinates are:\n");
-    for (int i = 0; i < num_dimensions; i++){
-        printf("%d ", points[p2].coordinates[i]);
+    // for each point in min_points1 print point1 and point2 /n
+    for (int i = 0; i < points1_count; i++){
+        printf("Point #%d: ", min_points1[i]);
+        for (int j = 0; j < num_dimensions; j++){
+            printf("%d ", points[min_points1[i]].coordinates[j]);
+        }
+        printf("Point #%d: ", min_points2[i]);
+        for (int j = 0; j < num_dimensions; j++){
+            printf("%d ", points[min_points2[i]].coordinates[j]);
+        }
+        printf("\n");
     }
 
     // free the memory
     free(points);
+    free(min_points1);
+    free(min_points2);
  
     return 0;
 }
