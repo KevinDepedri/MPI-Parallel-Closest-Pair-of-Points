b5f9093d6765f418b82a714fd634814932a825dc
Davide Modolo <davide.modolo98@gmail.com>
2023-01-26 18:41:38 +0100

    folder fixing

---
+++ parallel/utils/util.h
@@ -0,0 +1,12 @@
+typedef struct{
+    int *coordinates;
+    int num_dimensions;
+} Point;
+
+extern double distance(Point, Point );
+
+extern void merge(Point *, int, int, int, int);
+
+extern void mergeSortRec(Point *, int, int, int);
+
+extern void mergeSort(Point *, int, int);
