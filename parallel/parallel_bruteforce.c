c3eca61e32a55428cdb8659e9a5e5b71292ff78e
Davide Modolo <davide.modolo98@gmail.com>
2023-01-27 12:24:55 +0100

    indentation

--- parallel/parallel_bruteforce.c
+++ parallel/parallel_bruteforce.c
@@ -11,89 +11,100 @@ IDEA:
 - each cpu sends the minimum distance to the master
 - the master calculates the minimum of the minimums
 */
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
     int rank, size;
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    if(rank == 0){
-        Point* points;
-    int num_points, num_dimensions;
-    FILE* fp = fopen("../point_generator/1milion.txt", "r");
-    if (fp == NULL)
+    if (rank == 0)
     {
-        perror("Error opening file");
-        return 1;
-    }
-    // Read the number of points and dimensions from the first line of the file
-    fscanf(fp, "%d %d", &num_points, &num_dimensions);
-    if(num_points < 2){
-        printf("Error: the number of points must be greater than 1");
-        return 1;
-    }
-    // Allocate memory for the points
-    points = (Point *)malloc(num_points * sizeof(Point));
-    for (int i = 0; i < num_points; i++)
-    {
-        points[i].num_dimensions = num_dimensions;
-        points[i].coordinates = (int *)malloc(num_dimensions * sizeof(int));
-        for (int j = 0; j < num_dimensions; j++)
+        Point *points;
+        int num_points, num_dimensions;
+        FILE *fp = fopen("../point_generator/10thousands.txt", "r");
+        if (fp == NULL)
         {
-            fscanf(fp, "%d", &points[i].coordinates[j]);
+            perror("Error opening file");
+            return 1;
         }
-    }
-    fclose(fp);
+        // Read the number of points and dimensions from the first line of the file
+        fscanf(fp, "%d %d", &num_points, &num_dimensions);
+        if (num_points < 2)
+        {
+            printf("Error: the number of points must be greater than 1");
+            return 1;
+        }
+        // Allocate memory for the points
+        points = (Point *)malloc(num_points * sizeof(Point));
+        for (int i = 0; i < num_points; i++)
+        {
+            points[i].num_dimensions = num_dimensions;
+            points[i].coordinates = (int *)malloc(num_dimensions * sizeof(int));
+            for (int j = 0; j < num_dimensions; j++)
+            {
+                fscanf(fp, "%d", &points[i].coordinates[j]);
+            }
+        }
+        fclose(fp);
         // compute the number of points for each cpu
         int points_per_cpu = num_points / size;
         int points_left = num_points % size;
         // send the number of points to each cpu, with the index of the first point and all the points
         int first_point = 0;
-        for(int i = 1; i < size-1; i++){
+        for (int i = 1; i < size - 1; i++)
+        {
             MPI_Send(&points_per_cpu, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
             MPI_Send(&first_point, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
             MPI_Send(points + first_point, points_per_cpu * sizeof(Point), MPI_BYTE, i, 0, MPI_COMM_WORLD);
             first_point += points_per_cpu;
         }
         MPI_Send(&points_left, 1, MPI_INT, size - 1, 0, MPI_COMM_WORLD);
-        MPI_Send(&first_point, 1, MPI_INT, size-1, 0, MPI_COMM_WORLD);
-        MPI_Send(points + first_point, points_per_cpu * sizeof(Point), MPI_BYTE, size-1, 0, MPI_COMM_WORLD);
-
+        MPI_Send(&first_point, 1, MPI_INT, size - 1, 0, MPI_COMM_WORLD);
+        MPI_Send(points + first_point, points_per_cpu * sizeof(Point), MPI_BYTE, size - 1, 0, MPI_COMM_WORLD);
 
         // receive the minimum distance from each cpu
         double min_distance;
         double min_distance_cpu;
-        for(int i = 1; i < size; i++){
+        for (int i = 1; i < size; i++)
+        {
             MPI_Recv(&min_distance_cpu, 1, MPI_DOUBLE, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            if(min_distance_cpu < min_distance){
+            if (min_distance_cpu < min_distance)
+            {
                 min_distance = min_distance_cpu;
             }
         }
         printf("The minimum distance is: %f\n", min_distance);
         // free the memory
-        for (int i = 0; i < num_points; i++){
+        for (int i = 0; i < num_points; i++)
+        {
             free(points[i].coordinates);
         }
         free(points);
     }
-    else{
+    else
+    {
         // receive the number of points
         int num_points, first_point;
         MPI_Recv(&num_points, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         MPI_Recv(&first_point, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         // allocate memory for the points
-        Point* points = (Point *)malloc(num_points * sizeof(Point));
+        Point *points = (Point *)malloc(num_points * sizeof(Point));
         // receive the points
         MPI_Recv(points, num_points * sizeof(Point), MPI_BYTE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         // compute the minimum distance
         double min_distance = distance(points[first_point], points[0]);
-        
+
         // Find the minimum distance between any two points
-        for(int i = first_point; i < first_point + num_points; i++){
-            for(int j = 0; j < num_points; j++){
-                if(i != j){
+        for (int i = first_point; i < first_point + num_points; i++)
+        {
+            for (int j = 0; j < num_points; j++)
+            {
+                if (i != j)
+                {
                     double distance_ij = distance(points[i], points[j]);
-                    if(distance_ij < min_distance){
+                    if (distance_ij < min_distance)
+                    {
                         min_distance = distance_ij;
                     }
                 }
@@ -102,7 +113,8 @@ int main(int argc, char *argv[]){
         // send the minimum distance to the master
         MPI_Send(&min_distance, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
         // free the memory
-        for (int i = 0; i < num_points; i++){
+        for (int i = 0; i < num_points; i++)
+        {
             free(points[i].coordinates);
         }
         free(points);
