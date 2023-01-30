143e35e304b92862e3bf5e8e536eb6f22054167d
KevinDepedri <kevindepedri@hotmail.it>
2023-01-30 13:29:12 +0100

    fixed

--- parallel/parallel_mergesort.c
+++ parallel/parallel_mergesort.c
@@ -16,41 +16,6 @@ the master merges the parts
 #define AXIS 0
 #define MASTER_PROCESS 0
 
-
-// void sendPointsPacked(Point* points, int numPoints, int destination, int tag, MPI_Comm comm) {
-//   int bufferSize = numPoints * (sizeof(int) + sizeof(int)*points[0].num_dimensions);
-//   void *buffer = malloc(bufferSize);
-//   int position = 0;
-
-//   for (int point = 0; point < numPoints; point++) {
-//     MPI_Pack(&points[point].num_dimensions, 1, MPI_INT, buffer, bufferSize, &position, comm);
-//     MPI_Pack(points[point].coordinates, points[point].num_dimensions, MPI_INT, buffer, bufferSize, &position, comm);
-//   }
-
-//   MPI_Send(buffer, position, MPI_PACKED, destination, tag, comm);
-//   free(buffer);
-// }
-
-// void recvPointsPacked(Point* points, int numPoints, int source, int tag, MPI_Comm comm) {
-//   MPI_Status status;
-//   MPI_Probe(source, tag, comm, &status);
-
-//   int count;
-//   MPI_Get_count(&status, MPI_PACKED, &count);
-//   void* buffer = malloc(count);
-
-//   MPI_Recv(buffer, count, MPI_PACKED, source, tag, comm, MPI_STATUS_IGNORE);
-
-//   int position = 0;
-//   for (int i = 0; i < numPoints; i++) {
-//     MPI_Unpack(buffer, count, &position, &points[i].num_dimensions, 1, MPI_INT, comm);
-//     points[i].coordinates = (int*)malloc(points[i].num_dimensions * sizeof(int));
-//     MPI_Unpack(buffer, count, &position, points[i].coordinates, points[i].num_dimensions, MPI_INT, comm);
-//   }
-//   free(buffer);
-// }
-
-
 int main(int argc, char *argv[])
 {
     int rank_process, comm_size;
@@ -61,7 +26,7 @@ int main(int argc, char *argv[])
     int num_points, num_dimensions;
 
     //OPTIONS: ../point_generator/points_channel_with_image/points_channel.txt   ../point_generator/10thousands.txt   ../point_generator/1milion.txt
-    FILE *fp = fopen("../point_generator/points_channel_with_image/points_channel.txt", "r"); 
+    FILE *fp = fopen("../point_generator/1hundred.txt", "r"); 
     if (fp == NULL)
     {
         printf("Error opening file on core %d", rank_process);
@@ -83,12 +48,10 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    int num_points_normal_processes, num_points_local_process, points_in_excess, index_first_point, index_last_point = 0;
+    int num_points_normal_processes, num_points_local_process, index_first_point, index_last_point = 0;
 
     // Compute the number of points that will be read by each core
     num_points_normal_processes = num_points / (comm_size-1);
-    points_in_excess = num_points % (comm_size-1);
-    printf("%d", num_points_normal_processes);
     
     if (rank_process != MASTER_PROCESS)
     {
@@ -116,7 +79,8 @@ int main(int argc, char *argv[])
     // printf("HEADER SIZE: %ld\n", header_size);
 
     // Jump to the point of the file which need to be read by this core
-    long jump_size = 4 + 4 * 2 * (index_first_point); // The initial 4bytes is the size of the header (number of points and number of dimensions)
+    long jump_size = 4*2 + 4 * 2 * (index_first_point); // The initial 4bytes is the size of the header (number of points and number of dimensions)
+    printf("PROCESS %d: JUMP SIZE: %ld", rank_process, jump_size);
     if (fseek(fp, jump_size, SEEK_SET) != 0)
     {
         printf("Error: could not use fseek to perform jump inside the input point file\n");
@@ -145,47 +109,47 @@ int main(int argc, char *argv[])
     mergeSort(local_process_points, num_points_local_process, AXIS);
 
 
-    // Gather all the points in the main core
     if (rank_process == MASTER_PROCESS)
     {
+        // Gather all the points in the master process
         Point **processes_sorted_points;
         processes_sorted_points = (Point **)malloc(comm_size * sizeof(Point *));
 
-        points_in_excess = num_points % comm_size;
         processes_sorted_points[0] = (Point *)malloc((num_points_local_process) * sizeof(Point));
         for (int process = 1; process < comm_size; process++)
         {
             processes_sorted_points[process] = (Point *)malloc(num_points_normal_processes * sizeof(Point));
         }
 
-        // Save first the points ordered from the current core (core 0)
+        // Save first the points ordered from the current process (master), then receive the ones from the other processes
         processes_sorted_points[0] = local_process_points;
         for (int process = 1; process < comm_size; process++)
         {
             recvPointsPacked(processes_sorted_points[process], num_points_normal_processes, process, 0, MPI_COMM_WORLD);
         }
         printf("RECEIVE DONE\n");
-        print_points(processes_sorted_points[0], num_points_local_process, rank_process); 
-        print_points(processes_sorted_points[1], num_points_normal_processes, 1); 
-        print_points(processes_sorted_points[2], num_points_normal_processes, 2); 
-        printf("PRINT DONE\n");
 
         // Merge the points from all the cores
         Point *sorted_points;
         sorted_points = (Point *)malloc(num_points * sizeof(Point));
+        
         int *temporary_indexes;
         temporary_indexes = (int *)malloc(comm_size * sizeof(int));
         for (int process = 0; process < comm_size; process++)
         {
             temporary_indexes[process] = 0;
-        }
-        for (int i = 0; i < num_points; i++)
+        }  
+        printf("TEMP DONE\n");
+
+        for (int point = 0; point < num_points; point++)
         {
             int min = processes_sorted_points[0][temporary_indexes[0]].coordinates[AXIS];
             int process_with_minimum_value = 0;
+            printf("POINT DONE %d\n", point);
+
             for (int process = 0; process < comm_size; process++)
             {
-                if ((process < comm_size && temporary_indexes[process] < num_points_local_process) || (process == 0 && temporary_indexes[process] < num_points_local_process + points_in_excess))
+                if ((process < comm_size && temporary_indexes[process] < num_points_normal_processes) || (process == 0 && temporary_indexes[process] < num_points_local_process))
                 {
                     if (processes_sorted_points[process][temporary_indexes[process]].coordinates[AXIS] < min)
                     {
@@ -194,27 +158,14 @@ int main(int argc, char *argv[])
                     }
                 }
             }
-            sorted_points[i] = processes_sorted_points[process_with_minimum_value][temporary_indexes[process_with_minimum_value]];
+            sorted_points[point] = processes_sorted_points[process_with_minimum_value][temporary_indexes[process_with_minimum_value]];
             temporary_indexes[process_with_minimum_value]++;
         }
+        printf("REORDER DONE\n");
 
         // Print the sorted points ignoring verbose
-        for (int point = 0; point < num_points; point++)
-        {
-            printf("Point %d: (", point);
-            for (int dimension = 0; dimension < sorted_points[point].num_dimensions; dimension++)
-            {
-                if (dimension != sorted_points[point].num_dimensions - 1)
-                {
-                    printf("%d, ", sorted_points[point].coordinates[dimension]);
-                }
-                else
-                {
-                    printf("%d)\n", sorted_points[point].coordinates[dimension]);
-                }
-            }
-        }
-
+        printf("ORDERED POINTS:\n");
+        print_points(sorted_points, num_points, rank_process); 
     }
     else
     {
