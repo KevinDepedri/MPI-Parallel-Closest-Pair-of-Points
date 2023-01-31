58d7a3a20dad8b4d24997b9e77cac0cca56ec938
Davide Modolo <davide.modolo98@gmail.com>
2023-01-31 20:12:21 +0100

    parallel bruteforce DONE

--- parallel/parallel_bruteforce.c
+++ parallel/parallel_bruteforce.c
@@ -6,18 +6,32 @@
 #define INT_MAX 2147483647
 #include "utils/util.h"
 
+void printPoint(Point p)
+{
+    printf("(");
+    for (int i = 0; i < p.num_dimensions; i++)
+    {
+        printf("%d, ", p.coordinates[i]);
+    }
+    printf(")");
+}
+
 int main(int argc, char *argv[])
 {
     int rank, size;
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
-    
+
+    clock_t start, end;
+    double cpu_time_used;
+    start = clock();
+
     int num_points;
     Point *points;
     if (rank == 0)
     {
-        int num_points, num_dimensions;
+        int num_dimensions;
         FILE *fp = fopen("point_generator/points.txt", "r");
         if (fp == NULL)
         {
@@ -43,26 +57,33 @@ int main(int argc, char *argv[])
             }
         }
         fclose(fp);
-        printf("[R %d] Num points: %d\n", rank, num_points);
     }
     MPI_Bcast(&num_points, 1, MPI_INT, 0, MPI_COMM_WORLD);
-    printf("[R %d] Num points: %d\n", rank, num_points);
     int local_num, starting_index;
-    if (rank == 0){
-        for(int i = 1; i < size; i++){
+    if (rank == 0)
+    {
+        for (int i = 1; i < size; i++)
+        {
             sendPointsPacked(points, num_points, i, 1, MPI_COMM_WORLD);
         }
-        local_num = num_points % (size-1);
+        local_num = num_points % (size - 1);
         starting_index = num_points - local_num;
     }
-    else{
-        local_num = num_points / (size-1);
+    else
+    {
+        local_num = num_points / (size - 1);
         points = (Point *)malloc(num_points * sizeof(Point));
         recvPointsPacked(points, num_points, 0, 1, MPI_COMM_WORLD);
         starting_index = (rank - 1) * local_num;
     }
     double min_distance = INT_MAX;
-    if(local_num > 0){
+    int *min_points1 = (int *)malloc(num_points * sizeof(int));
+    int *min_points2 = (int *)malloc(num_points * sizeof(int));
+    int points1_count = 0;
+    int points2_count = 0;
+    int count = 0;
+    if (local_num > 0)
+    {
         for (int i = starting_index; i < starting_index + local_num; i++)
         {
             for (int j = i + 1; j < num_points; j++)
@@ -71,17 +92,53 @@ int main(int argc, char *argv[])
                 if (dd < min_distance)
                 {
                     min_distance = dd;
+                    min_points1[0] = i;
+                    min_points2[0] = j;
+                    count = 1;
+
+                    points1_count = 1;
+                    points2_count = 1;
+                }
+                else if (dd == min_distance)
+                {
+                    min_points1[count] = i;
+                    min_points2[count] = j;
+                    count++;
+                    points1_count++;
+                    points2_count++;
                 }
             }
         }
     }
     double global_min;
     MPI_Allreduce(&min_distance, &global_min, 1, MPI_DOUBLE, MPI_MIN, MPI_COMM_WORLD);
+    if (min_distance == global_min)
+    {
+        printf("[R %d] Points that got the minimum distance:\n", rank);
+        for (int i = 0; i < points1_count; i++)
+        {
+            printPoint(points[min_points1[i]]);
+            printf(" - ");
+            printPoint(points[min_points2[i]]);
+            printf("\n");
+        }
+    }
     if (rank == 0)
     {
         printf("The minimum distance is %f\n", global_min);
+        end = clock();
+        cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+        printf("Time elapsed: %f\n", cpu_time_used);
     }
 
+    // Free the memory
+    for (int i = 0; i < num_points; i++)
+    {
+        free(points[i].coordinates);
+    }
+    free(points);
+    free(min_points1);
+    free(min_points2);
 
     MPI_Finalize();
     return 0;
