949214ab4ff36b3e75915ef75e87031665368733
KevinDepedri <kevindepedri@hotmail.it>
2023-02-03 09:14:03 +0100

    fixing stuff

--- sequential/compile_all.sh
+++ sequential/compile_all.sh
@@ -1,3 +1,3 @@
-# File to gcc compile sequential_XXX.c and utils/util.c together
-gcc -o sequential_bruteforce .\sequential_bruteforce.c utils/util.c
-gcc -o sequential_recursive .\sequential_recursive.c utils/util.c
+# File to gcc compile sequential_XXX.c and utils/util.c together. WARNING: if executing this files leads to error try the commands one at time
+gcc -g -Wall -o sequential_bruteforce sequential_bruteforce.c utils/util.c -lm
+gcc -g -Wall -o sequential_recursive sequential_recursive.c utils/util.c -lm
