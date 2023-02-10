55d5cb84cfc191b99f8b8a0b177b81197b744dfc
KevinDepedri <kevindepedri@hotmail.it>
2023-02-10 09:24:14 +0100

    running on 1 and 2 cores

--- runs/parallel/recursive/packed/start_parallel_recursive_packed_1core.sh
+++ runs/parallel/recursive/packed/start_parallel_recursive_packed_1core.sh
@@ -2,10 +2,10 @@
 #PBS -l select=1:ncpus=1:mem=5gb -l place=pack
 
 # Set max execution time
-#PBS -l walltime=24:00:00
+#PBS -l walltime=6:00:00
 
 # Set the excution on the short queue
-#PBS -q long_cpuQ
+#PBS -q short_cpuQ
 
 # Set name of job, output and error file
 #PBS -N Rec1CorePacked
@@ -14,4 +14,4 @@
 
 # Load the library and execute the parallel application
 module load mpich-3.2
-mpiexec -n 1 /home/kevin.depedri/hpc3/runs/parallel/recursive/mpi_recursive_long_2cores /home/kevin.depedri/points/250M5d.txt -e -p
+mpiexec -n 1 /home/kevin.depedri/hpc3/runs/parallel/recursive/mpi_recursive_long_2cores /home/kevin.depedri/points/250M5d.txt
--- runs/parallel/recursive/packed_exclusive/start_parallel_recursive_packed_excl_1core.sh
+++ runs/parallel/recursive/packed_exclusive/start_parallel_recursive_packed_excl_1core.sh
@@ -2,10 +2,10 @@
 #PBS -l select=1:ncpus=1:mem=5gb -l place=pack:excl
 
 # Set max execution time
-#PBS -l walltime=24:00:00
+#PBS -l walltime=6:00:00
 
 # Set the excution on the short queue
-#PBS -q long_cpuQ
+#PBS -q short_cpuQ
 
 # Set name of job, output and error file
 #PBS -N Rec1CorePackedExcl
@@ -14,4 +14,4 @@
 
 # Load the library and execute the parallel application
 module load mpich-3.2
-mpiexec -n 1 /home/kevin.depedri/hpc3/runs/parallel/recursive/mpi_recursive_long_2cores /home/kevin.depedri/points/250M5d.txt -e -p
+mpiexec -n 1 /home/kevin.depedri/hpc3/runs/parallel/recursive/mpi_recursive_long_2cores /home/kevin.depedri/points/250M5d.txt
